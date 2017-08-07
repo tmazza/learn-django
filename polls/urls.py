7104e7ad4c20c6f7482048f66922da04d0488171
tmazza <tiagomdepaula@gmail.com>
2017-08-07 12:03:45 -0300

    part 4

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+  # /polls/
+  url(r'^$', views.IndexView.as_view(), name='index'),
+  # /polls/4
+  url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+  # /polls/4/results
+  url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+  # /polls/4/vote
+  url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
 
-def index(request):
-	return HttpResponse("Sweet Honey!")
+from .models import Question, Choice
+
+class IndexView(generic.ListView):
+  template_name = 'polls/index.html'
+  context_object_name = 'questions'
+
+  def get_queryset(seld):
+    return Question.objects.order_by('-pub_data')[:5]
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = 'polls/results.html'
+
+def vote(request, question_id):
+  q = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = q.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question': q,
+      'error_message': "selecion certo",
+    })
+  else:
+    selected_choice.votes = F('votes') +1
+    selected_choice.save()    
+
+    return HttpResponseRedirect(reverse('polls:results', args=(q.id,)))
