4ebe342ccbe2b7dfb3abd9fde72f5db75d1e2b65
tmazza <tiagomdepaula@gmail.com>
2017-08-07 15:41:08 -0300

    end'

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
 
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+  list_display = ('question_text', 'pub_data', 'was_publish_recently')
+  fieldsets = [
+    (None,          {'fields': ['question_text']}),
+    ('Datas',       {'fields': ['pub_data'], 'classes': ['collapse'] }),
+  ]
+  inlines = [ChoiceInline]
+  list_filter = ['pub_data']
+  search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -5,13 +5,17 @@ from django.utils import timezone
 
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
-  pub_data = models.DateTimeField('date published')
+  pub_data = models.DateTimeField('Data de publicação')
 
   def __str__(self):
     return self.question_text
 
   def was_publish_recently(self):
     return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
+
+  was_publish_recently.admin_order_field = 'pub_data'
+  was_publish_recently.boolean = True
+  was_publish_recently.short_description = 'on the top \\o/'
 
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
